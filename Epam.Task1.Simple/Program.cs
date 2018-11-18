56332706fdb1ec3d7bf497863a3dbed4e1b9a119
SelinaIrra <irra.selina@mail.ru>
2018-11-18 23:07:03 +0400

    add "Simple"

--- Epam.Task1.Simple/Program.cs
+++ Epam.Task1.Simple/Program.cs
@@ -8,8 +8,20 @@ namespace Epam.Task1.Simple
 {
     class Program
     {
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i * i <= n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            if (IsPrime(n))
+                Console.WriteLine("it is prime number");
+            else
+                Console.WriteLine("it is not prime number");
         }
     }
 }
