b186c9eddab313f4a997210094d5298a68feb839
SelinaIrra <irra.selina@mail.ru>
2018-11-18 23:21:17 +0400

    add "Square"

--- Epam.Task1.Square/Program.cs
+++ Epam.Task1.Square/Program.cs
@@ -8,8 +8,21 @@ namespace Epam.Task1.Square
 {
     class Program
     {
+        static void PrintSquare(int n)
+        {
+            int center = n / 2;
+            for (int i = 0; i < n; i++, Console.WriteLine())
+                for (int j = 0; j < n; j++)
+                    if (i == center && j == center)
+                        Console.Write(' ');
+                    else
+                        Console.Write('*');
+        }
+
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            PrintSquare(n);
         }
     }
 }
