803e07c5e40bae83dbfbd044ee51f0bfd65f4748
SelinaIrra <irra.selina@mail.ru>
2018-11-18 22:52:53 +0400

    add "Sequence"

--- Epam.Task1.Sequence/Program.cs
+++ Epam.Task1.Sequence/Program.cs
@@ -8,8 +8,22 @@ namespace Epam.Task1.Sequence
 {
     class Program
     {
+        static string Sequence(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < n; i++)
+            {
+                sb.Append(i);
+                sb.Append(", ");
+            }
+            sb.Append(n);
+            return sb.ToString();
+        }
+
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Sequence(n));
         }
     }
 }
