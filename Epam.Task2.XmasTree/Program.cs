b9d62216b2b12a5d55abbbdcc4182dc7cc394987
SelinaIrra <irra.selina@mail.ru>
2018-11-23 21:47:08 +0400

    Add Epam.Task2.XmasTree

---
+++ Epam.Task2.XmasTree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.XmasTree
+{
+    class Program
+    {
+        static void PrintTree(uint n)
+        {
+            for (uint i = 1; i <= n; i++)
+                for (uint j = 1; j <= i; j++, Console.WriteLine())
+                {
+                    for (uint k = 0; k < n - j; k++)
+                        Console.Write(" ");
+                    for (uint k = 0; k < j * 2 - 1; k++)
+                        Console.Write("*");
+                    for (uint k = 0; k < n - j; k++)
+                        Console.Write(" ");
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            if (UInt32.TryParse(Console.ReadLine(), out uint n))
+                PrintTree(n);
+            else
+                Console.WriteLine("Incorrect input");
+        }
+    }
+}
