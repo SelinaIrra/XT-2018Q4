2db4952cae02497c4988c9e80748bdd58b2078e9
SelinaIrra <irra.selina@mail.ru>
2018-11-23 21:28:16 +0400

    Add Epam.Task2.AnotherTriangle

---
+++ Epam.Task2.AnotherTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.AnotherTriangle
+{
+    class Program
+    {
+        static void PrintTriangle(uint n)
+        {
+            for (uint i = 1; i <= n; i++, Console.WriteLine())
+            {
+                for (uint j = 0; j < n - i; j++)
+                    Console.Write(" ");
+                for (uint j = 0; j < i * 2 - 1; j++)
+                    Console.Write("*");
+                for (uint j = 0; j < n - i; j++)
+                    Console.Write(" ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (UInt32.TryParse(Console.ReadLine(), out uint n))
+                PrintTriangle(n);
+            else
+                Console.WriteLine("Incorrect input");
+        }
+    }
+}
