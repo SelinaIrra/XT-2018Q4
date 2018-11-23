a42e89fe4e73ef2ae98c66562da17640f33bb03c
SelinaIrra <irra.selina@mail.ru>
2018-11-23 21:56:46 +0400

    Add Epam.Task2.SumOfNumbers

---
+++ Epam.Task2.SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.SumOfNumbers
+{
+    class Program
+    {
+        static uint Sum(uint div)
+        {
+            uint ans = 0;
+            for (uint i = div; i < 1000; i += div)
+                ans += i;
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(3) + Sum(5) - Sum(15));
+        }
+    }
+}
