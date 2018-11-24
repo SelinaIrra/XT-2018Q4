f3ab7e0ea5b6bf761d57baf4a6d46ed5eebea07d
SelinaIrra <irra.selina@mail.ru>
2018-11-24 17:38:41 +0400

    add Epam.Task2.NonNegativeSum

---
+++ Epam.Task2.NonNegativeSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.NonNegativeSum
+{
+    class Program
+    {
+        static void PrintArray(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]}  ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int n = random.Next(4, 10);
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+                array[i] = random.Next(-250, 250);
+            Console.WriteLine("Generated array:");
+            PrintArray(ref array);
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                if (array[i] > 0)
+                    sum += array[i];
+            Console.WriteLine($"Non-negative sum: {sum}");
+        }
+    }
+}
