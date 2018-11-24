01bd20480b2ea9e250eaf50ba50eb3ef1a84fe44
SelinaIrra <irra.selina@mail.ru>
2018-11-24 18:08:39 +0400

    Add Epam.Task2.AverageStringLength

---
+++ Epam.Task2.AverageStringLength/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.AverageStringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int currentWordLength = 0,
+                lettersCount = 0,
+                wordsCount = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsSeparator(text, i) && currentWordLength > 0)
+                {
+                    lettersCount += currentWordLength;
+                    currentWordLength = 0;
+                    wordsCount++;
+                }
+                else if (Char.IsLetter(text, i))
+                    currentWordLength++;
+            }
+            if (currentWordLength > 0)
+            {
+                lettersCount += currentWordLength;
+                wordsCount++;
+            }
+            Console.WriteLine($"AVG word length: {(Double)lettersCount / wordsCount}");
+        }
+    }
+}
