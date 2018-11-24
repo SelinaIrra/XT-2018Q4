300e0337dd536c9501534c7b1d8de9ae63531301
SelinaIrra <irra.selina@mail.ru>
2018-11-24 18:17:40 +0400

    add Epam.Task2.CharDoubler

---
+++ Epam.Task2.CharDoubler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.CharDoubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string s2 = Console.ReadLine();
+            Console.Write("Результирующая строка: ");
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Console.Write(s1[i]);
+                if (s2.Contains(s1[i]))
+                    Console.Write(s1[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
